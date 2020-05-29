aecb55a38a8388ff840bb77defa6dcbd8c228309
ambreshbiradar9 <ambreshbiradar9@gmail.com>
2020-05-29 09:09:53 +0530

    Day_6 yet to be changed

--- week1/Day_6.cpp
+++ week1/Day_6.cpp
@@ -1,68 +1,25 @@
-#include<iostream>
-#include<vector>
-struct Node
-{
-    int val;
-    Node *right;
-    Node *left;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() :
+        val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) :
+        val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) :
+        val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) 
+    {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);    
+        while(!queue.isEmpty)
+        TreeNode *temp;
+        temp=root;
+    }
 };
-
-Node *root=NULL;
-void insert(int data)
-{
-    Node *temp1;
-    while(temp1!=NULL)
-    {
-        if(data>temp1->val)
-        {
-            temp1=temp1->right;
-        }
-        else
-        {
-            temp1=temp1->left;
-        }
-    }
-    Node *temp=new Node();
-    temp->val=data;
-    temp->right=temp->right=NULL;
-    temp1=temp;
-}
-
-int maxof(int m1,int m2)
-{
-    if(m1<m2)
-    {
-        return m2;
-    }
-    else
-    {
-        return m1;
-    }   
-}
-
-int height(Node* temp)
-{
-if (temp==NULL)
-return 0;
-else
-{
-    return  (1+maxof(height(temp->right),height(temp->left)));
-}
-
-}
-
-int main()
-{
-    int n,temp;
-    //std::vector <int> c;
-    std::cin >>  n;
-    for(int i=0;i<n;i++)
-    {
-        std::cin>>temp;
-      //  c.push_back(temp);
-        insert(temp);
-    }
-    std::cout<<height(root);
-    return 1;
-}
-
